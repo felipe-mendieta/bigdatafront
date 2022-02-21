2a6ddcd13fb2d96f5378306bb4d36d6c267ee79b
felipe-mendieta <48095820+felipe-mendieta@users.noreply.github.com>
2022-02-21 11:28:12 -0500

    Estilos pantalla principal
    Se modifico la pantalla principal, no se modifico nada relacionado con la logica del programa, solo vistas.

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
-
+  title: string="BIG DATA ARQUITECTURA LAMBDA"
   constructor() { }
 
   ngOnInit(): void {
